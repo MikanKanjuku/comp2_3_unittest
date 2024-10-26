3b117e37fc2cffead27819bf1b6947cfe0398ff5
MikanKanjuku <mikanna1014@gmail.com>
2024-10-26 16:32:42 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -21,6 +21,10 @@ int sum(int num, int* array)
 // num個の整数の平均を得る
 int average(int num, int* array)
 {
-	
-	return (2 + 4 )/ 2;// ■未実装
+	int sum = 0;
+	for (int i = 0; i < num; i++) {
+		sum += array[i];  // 配列の各要素を合計
+	}
+
+	return sum / num;  // 合計をnumで割って平均を返す
 }
